2b1a746d634374b4e30b3798f9df55a191e67416
Gede Wiguna <wiguna.gd@gmail.com>
2026-05-04 12:12:02 +0800

    2026-05-04

--- src/app/(commonfunctions)/hooksHomepage.ts
+++ src/app/(commonfunctions)/hooksHomepage.ts
@@ -14,6 +14,7 @@ export const useGetFeed = (params: RequestParamLimitPage) => {
     });
 }
 
+
 export const useGetSaved = (params: RequestParamLimitPage) => {
     return useInfiniteQuery<FeedResponse, AxiosError>({
         initialPageParam: 1,
